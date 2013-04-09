8172ba8d452f76f391a11288edd5d8ada589b918
liy <liy8272@gmail.com>
2013-04-09 22:45:45 +0100

    spot light

---
+++ shader_struct-2013_04_06/Loader.js
@@ -0,0 +1,23 @@
+(function(window){
+  function Loader(){
+
+  }
+  var p = Loader.prototype;
+
+  p.load = function(path, callback, async){
+    if(async == undefined) async = true;
+    console.log(async);
+    this.callback = callback;
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', path, async);
+    xhr.onload = bind(this, this.onload);
+    xhr.send();
+  }
+
+  p.onload = function(e){
+    this.data = e.target.responseText;
+    this.callback(e);
+  }
+
+  window.Loader = Loader;
+})(window)
---
+++ spotlight-2013_04_09/default.vert
@@ -0,0 +1,18 @@
+attribute vec3 a_Vertex;
+attribute vec3 a_Normal;
+attribute vec2 a_TexCoord;
+
+uniform mat4 u_ProjectionMatrix;
+uniform mat4 u_ModelViewMatrix;
+uniform mat3 u_NormalMatrix;
+
+varying vec4 v_Vertex;
+varying vec3 v_Normal;
+varying vec2 v_TexCoord;
+
+void main(){
+  v_Vertex = u_ModelViewMatrix * vec4(a_Vertex, 1.0);
+  gl_Position = u_ProjectionMatrix * v_Vertex;
+  v_Normal = u_NormalMatrix * a_Normal;
+  v_TexCoord = a_TexCoord;
+}
