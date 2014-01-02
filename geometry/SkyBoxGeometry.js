bd8bb026d21c4febaffa4c16f8ce89119a1d5413
liy <liy8272@gmail.com>
2014-01-02 06:46:20 +0000

    skybox

---
+++ geometry/SkyBoxGeometry.js
@@ -0,0 +1,90 @@
+function SkyBoxGeometry(width, height, depth){
+  Geometry.call(this);
+
+  this.width = width ? width : 1;
+  this.height = height ? height : 1;
+  this.depth = depth ? depth : 1;
+
+  var hw = this.width/2;
+  var hh = this.height/2;
+  var hd = this.depth/2;
+
+  this.numVertices = 24;
+
+  this.vertices = [
+    // x y z   nx ny nz
+    // front
+    -hw, -hh,  hd,
+     hw, -hh,  hd,
+     hw,  hh,  hd,
+    -hw,  hh,  hd,
+    // back
+     hw, -hh, -hd,
+    -hw, -hh, -hd,
+    -hw,  hh, -hd,
+     hw,  hh, -hd,
+    // top
+    -hw,  hh,  hd,
+     hw,  hh,  hd,
+     hw,  hh, -hd,
+    -hw,  hh, -hd,
+    // bottom
+    -hw, -hh, -hd,
+     hw, -hh, -hd,
+     hw, -hh,  hd,
+    -hw, -hh,  hd,
+    // left
+    -hw, -hh, -hd,
+    -hw, -hh,  hd,
+    -hw,  hh,  hd,
+    -hw,  hh, -hd,
+    // right
+     hw, -hh,  hd,
+     hw, -hh, -hd,
+     hw,  hh, -hd,
+     hw,  hh,  hd,
+  ];
+
+  this.texCoords = [
+    // front
+    0.0, 0.0,  1.0,
+    1.0, 0.0,  1.0,
+    1.0, 1.0,  1.0,
+    0.0, 1.0,  1.0,
+    // back
+    0.0, 0.0,  0.0,
+    1.0, 0.0,  0.0,
+    1.0, 1.0,  0.0,
+    0.0, 1.0,  0.0,
+    // top
+    0.0, 0.0,  1.0,
+    1.0, 0.0,  1.0,
+    1.0, 1.0,  1.0,
+    0.0, 1.0,  1.0,
+    // bottom
+    0.0, 0.0,  0.0,
+    1.0, 0.0,  0.0,
+    1.0, 1.0,  0.0,
+    0.0, 1.0,  0.0,
+    // left
+    0.0, 0.0,
+    1.0, 0.0,
+    1.0, 1.0,
+    0.0, 1.0,
+    // right
+    0.0, 0.0,
+    1.0, 0.0,
+    1.0, 1.0,
+    0.0, 1.0
+  ];
+
+  // index information
+  this.indices = [
+    0,  1,  2,   0,  2,  3,  // front
+    4,  5,  6,   4,  6,  7,  // back
+    8,  9,  10,  8,  10, 11, // top
+    12, 13, 14,  12, 14, 15, // bottom
+    16, 17, 18,  16, 18, 19, // left
+    20, 21, 22,  20, 22, 23  // right
+  ];
+}
