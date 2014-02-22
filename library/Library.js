cdf8a903e10a9c2848d00a080232bf1440dd8e33
liy <liy8272@gmail.com>
2014-02-22 18:34:01 +0000

    Planning to use grunt and traceur

--- library/Library.js
+++ library/Library.js
@@ -1,43 +1,42 @@
-// "use strict"
+(function(window){
+  "use strict"
 
-// (function(window){
+  window.Library = {
+    init: function(){
+      // default resourceStore
+      var resourceStore = Library.resourceStore = {};
 
-//   Library = Object.create(null);
-  
-//   // default resourceStore
-//   var resourceStore = Library.resourceStore = {};
-
-//   /**
-//    * Image resource key has it corresponding extension
-//    * Shader resource key extension is shader
-//    * Sound resource key will be have its corresponding extension
-//    */
-//   resourceStore = {
-//     'posx.jpg': new ImageResource('../webgl-meshes/cube_map/posx.jpg'),
-//     'negx.jpg': new ImageResource('../webgl-meshes/cube_map/negx.jpg'),
-//     'posy.jpg': new ImageResource('../webgl-meshes/cube_map/posy.jpg'),
-//     'negy.jpg': new ImageResource('../webgl-meshes/cube_map/negy.jpg'),
-//     'posz.jpg': new ImageResource('../webgl-meshes/cube_map/posz.jpg'),
-//     'negz.jpg': new ImageResource('../webgl-meshes/cube_map/negz.jpg'),
+      /**
+       * Image resource key has it corresponding extension
+       * Shader resource key extension is shader
+       * Sound resource key will be have its corresponding extension
+       */
+      resourceStore = {
+        'posx.jpg': new ImageResource('../webgl-meshes/cube_map/posx.jpg'),
+        'negx.jpg': new ImageResource('../webgl-meshes/cube_map/negx.jpg'),
+        'posy.jpg': new ImageResource('../webgl-meshes/cube_map/posy.jpg'),
+        'negy.jpg': new ImageResource('../webgl-meshes/cube_map/negy.jpg'),
+        'posz.jpg': new ImageResource('../webgl-meshes/cube_map/posz.jpg'),
+        'negz.jpg': new ImageResource('../webgl-meshes/cube_map/negz.jpg'),
 
 
-//     'directional.shader': new Shader('shader/light/directional.vert', 'shader/light/directional.frag'),
-//     'point.shader': new Shader('shader/light/point.vert', 'shader/light/point.frag'),
+        'directional.shader': new Shader('shader/light/directional.vert', 'shader/light/directional.frag'),
+        'point.shader': new Shader('shader/light/point.vert', 'shader/light/point.frag'),
 
-//     'geometry.shader': new Shader('shader/geometry.vert', 'shader/geometry.frag'),
-//     'screen.shader': new Shader('shader/screen.vert', 'shader/screen.frag'),
-//     'skybox.shader': new Shader('shader/skybox.vert', 'shader/skybox.frag'),
-//     'stencil.shader': new Shader('shader/stencil.vert', 'shader/stencil.frag'),
-//     'synthesis.shader': new Shader('shader/synthesis.vert', 'shader/synthesis.frag'),
+        'geometry.shader': new Shader('shader/geometry.vert', 'shader/geometry.frag'),
+        'screen.shader': new Shader('shader/screen.vert', 'shader/screen.frag'),
+        'skybox.shader': new Shader('shader/skybox.vert', 'shader/skybox.frag'),
+        'stencil.shader': new Shader('shader/stencil.vert', 'shader/stencil.frag'),
+        'synthesis.shader': new Shader('shader/synthesis.vert', 'shader/synthesis.frag'),
 
-//     'probe_debug.shader': new Shader('shader/probe/probe_debug.vert', 'shader/probe/probe_debug.frag'),
-//     'probe_geometry.shader': new Shader('shader/probe/probe_geometry.vert', 'shader/probe/probe_geometry.frag'),
-//     'probe_synthesis.shader': new Shader('shader/probe/probe_synthesis.vert', 'shader/probe/probe_synthesis.frag')
-//   };
+        'probe_debug.shader': new Shader('shader/probe/probe_debug.vert', 'shader/probe/probe_debug.frag'),
+        'probe_geometry.shader': new Shader('shader/probe/probe_geometry.vert', 'shader/probe/probe_geometry.frag'),
+        'probe_synthesis.shader': new Shader('shader/probe/probe_synthesis.vert', 'shader/probe/probe_synthesis.frag')
+      };
+    }
+  }
 
-//   window.Library = Library;
-
-// })(window);
+})(window);
 
 
 
--- main.js
+++ main.js
@@ -1,17 +1,1 @@
 "use strict"
-var stats = new Stats();
-stats.setMode(0); // 0: fps, 1: ms
-// Align top-left
-stats.domElement.style.position = 'absolute';
-stats.domElement.style.left = '0px';
-stats.domElement.style.top = '0px';
-document.body.appendChild( stats.domElement );
-
-var renderer = new DeferredRenderer();
-
-var file = new ObjectFile();
-file.load('../webgl-meshes/head/head.obj').then(function(loader){
-  console.log('loaded', loader)
-}).catch(function(error){
-  console.log('error');
-})
